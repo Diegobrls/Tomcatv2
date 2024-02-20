e60ef7aa2db9fc4a5d34e30072395966eb752846
Diego Sanchez Briales <105086398+Diegobrls@users.noreply.github.com>
2024-02-20 21:11:00 +0100

    Create pila.sh

---
+++ pila.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+STACK_NAME=tomcat-stack
+TEMPLATE_FILE=ubuntu.yml
+
+aws cloudformation deploy --stack-name $STACK_NAME --template-file $TEMPLATE_FILE --capabilities CAPABILITY_IAM
+
+# Obtener URL de la aplicacion
+if [ $? -eq 0 ]; then
+    aws cloudformation list-exports \
+        --query "Exports[?Name=='IPaddress'].Value"
+fi
